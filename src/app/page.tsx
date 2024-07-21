2c34e99e603967d001c107d1b9fa5a9704dd64db
Ishmam Ahmed <ishmam.ahmed24@gmail.com>
2024-07-21 23:42:19 +0100

    Final updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,55 @@
-import Image from "next/image";
+import Link from "next/link";
 
-export default function Home() {
+const Landing = () => {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <section
+        id="landing"
+        className="landing-background relative z-10 grid place-items-center min-h-screen bg-black text-white"
+      >
+        <div className="container mx-auto">
+          <div className="grid grid-cols-1 lg:grid-cols-2 items-center">
+            <img className="p-10 lg:p-16" src="/hero.png" alt="landing" />
+            <div className="landingForm justify-center">
+              <div className="justify-center">
+                <h2 className="text-4xl font-bold mb-1 text-[#923c4a] lg:text-5xl">Bangladesh</h2>
+                <h2 className="maintitle text-[#923c4a] mb-3 text-2xl font-bold lg:text-3xl">Is under state repression, violence, and extrajudicial killing</h2>
+                <div className="mb-5 text-base leading-relaxed text-white sm:text-lg md:text-xl max-w-3xl ">
+                  <ul className="mb-3 text-base leading-relaxed sm:text-lg md:text-xl max-w-3xl list-disc list-inside">
+                    <li>200+ students and civilians killed for protesting against the 56% reserved quota in civil service jobs.</li>
+                    <li>Telecommunication blockage for over 72 hours and counting.</li>
+                    <li>Shoot-on-sight order during indefinite curfew.</li>
+                  </ul>
+                  The Bangladesh Awami League (BAL), in power for nearly two decades, leveraged liberation war sentiment and reserved <b className="text-[#923c4a]">one-third of government jobs for war-hero families</b>,
+                  sparking protests over discrimination amid rising unemployment. Nationwide unrest followed Premier Sheikh Hasina's labelling of protestors
+                  as <b className="text-[#923c4a]">national traitors</b>, resulting in <b className="text-[#923c4a]">hundreds killed</b> and thousands injured by the police and the pro-BAL student wing activists, leading to
+                  a <b className="text-[#923c4a]">shoot-on-sight order</b>, indefinite <b className="text-[#923c4a]">curfew</b>, and internet <b className="text-[#923c4a]">outage</b>. Disregarding the people's voices, the ruling party is proven <b className="text-[#923c4a]">zero accountable</b> for the public good and should step down.
+                  The citizens of Bangladesh deserve justice, equality, accountability, and prosperity.
+                  <p className="mt-5 font-bold text-[#923c4a]">
+                    #SaveBangladeshiStudents | #StopKillingStudents | #StepDownHasina
+                  </p>
+                  <p className="mt-1 text-[#585858]">
+                    - Leeds Bangladesh Community
+                  </p>
+                  <p className="mt-5 mb-5 text-base leading-relaxed text-white sm:text-lg md:text-xl max-w-3xl">
+                    Sign the petition below to support our cause and demand justice.
+                  </p>
+                </div>
+                <Link href="https://chng.it/PstRX7qTrx">
+                  <button
+                    type="button"
+                    className="w-full rounded bg-[#923c4a] px-6 py-3 text-xs font-medium uppercase leading-normal text-white shadow-md transition duration-150 ease-in-out hover:bg-red-950 hover:text-[#white] focus:bg-white focus:text-[#923c4a] focus:outline-none focus:ring-2 focus:ring-white active:bg-[#923c4a] active:text-white"
+                  >
+                    Sign the Petition
+                  </button>
+                </Link>
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      </section>
+    </>
   );
-}
+};
+
+export default Landing;
