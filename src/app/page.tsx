7d0ab1de5a9a23800aa121fd8e0636161dc4f2c2
Ishmam Ahmed <ishmam.ahmed24@gmail.com>
2024-07-23 22:59:09 +0100

    Updated info

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,16 +17,17 @@ const Landing = () => {
                 <h2 className="maintitle text-[#923c4a] mb-3 text-2xl font-bold lg:text-3xl">
                   Is under state repression, violence, and extrajudicial killing
                 </h2>
-                <div className="mb-5 text-base leading-relaxed text-white sm:text-lg md:text-xl max-w-3xl ">
+                <div className="mb-5 text-base leading-relaxed text-white sm:text-lg md:text-xl max-w-3xl">
                   <ul className="mb-3 text-base leading-relaxed sm:text-lg md:text-xl max-w-3xl list-disc list-inside">
                     <li>200+ students and civilians killed for protesting against the 56% reserved quota in civil service jobs.</li>
                     <li>Telecommunication blockage for over 72 hours and counting.</li>
                     <li>Shoot-on-sight order during indefinite curfew.</li>
                   </ul>
+                  The peaceful quota reform movement, initially advocating for equal job opportunities, escalated into nationwide unrest due to the government's harsh response.
                   The Bangladesh Awami League (BAL), in power for nearly two decades, leveraged liberation war sentiment and reserved <b className="text-[#923c4a]">one-third of government jobs for war-hero families</b>,
-                  sparking protests over discrimination amid rising unemployment. Nationwide unrest followed Premier Sheikh Hasina&apos;s labelling of protestors
+                  sparking protests over discrimination amid rising unemployment. Nationwide unrest followed Premier Sheikh Hasina's labelling of protestors
                   as <b className="text-[#923c4a]">national traitors</b>, resulting in <b className="text-[#923c4a]">hundreds killed</b> and thousands injured by the police and the pro-BAL student wing activists, leading to
-                  a <b className="text-[#923c4a]">shoot-on-sight order</b>, indefinite <b className="text-[#923c4a]">curfew</b>, and internet <b className="text-[#923c4a]">outage</b>. Disregarding the people&apos;s voices, the ruling party is proven <b className="text-[#923c4a]">zero accountable</b> for the public good and should step down.
+                  a <b className="text-[#923c4a]">shoot-on-sight order</b>, indefinite <b className="text-[#923c4a]">curfew</b>, and internet <b className="text-[#923c4a]">outage</b>. Disregarding the people's voices, the ruling party is proven <b className="text-[#923c4a]">zero accountable</b> for the public good and should step down.
                   The citizens of Bangladesh deserve justice, equality, accountability, and prosperity.
                   <p className="mt-5 font-bold text-[#923c4a]">
                     #SaveBangladeshiStudents | #StopKillingStudents | #StepDownHasina
